98691250f25ef436c24143121495b0b852191868
Noermerich <rene.noermerich@stud.thga.de>
2025-05-26 09:24:06 +0200

    Add C solution print.c for Aufgabe 3

---
+++ solutions/print.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello from C!\n");
+
+    // Beispiel: Formatierte Ausgabe
+    printf("Integer: %d, Float: %.2f, String: %s\n", 42, 3.14, "test");
+
+    // puts schreibt einfach einen String mit Zeilenumbruch
+    puts("This is puts output");
+
+    return 0;
+}
+
