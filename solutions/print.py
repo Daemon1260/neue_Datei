c82080e24f38c0a2fd2ff7c893e062496f6b4319
Noermerich <rene.noermerich@stud.thga.de>
2025-05-26 09:40:36 +0200

    Add print.py für Aufgabe 4

---
+++ solutions/print.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def main():
+    print("Hello from Python3!")
+    print("A", "B", "C", sep="-", end=".\n")  # gibt "A-B-C." aus
+    value = 2 + 2
+    print(f"Two plus two equals {value}")    # F-String
+
+if __name__ == "__main__":
+    main()
+
